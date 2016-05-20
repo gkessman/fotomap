8465f099e688f095e7d93af6130012007bc4852d
Greg Kessman <greg.kessman@gmail.com>
2016-05-20 13:47:40 -0400

    remove public-ip module

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var request = require('request');
 var favicon = require('serve-favicon');
-const publicIp = require('public-ip');
 var app = express();
 var port = process.env.PORT || 3000;
 var token = '2259798362.1fb234f.b87adebc5dfd47f8a55adae001f63ca2';
@@ -14,17 +13,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/photos', function(req, res) {
-	console.log('ip address: ', req.connection.remoteAddress)
 	var reqInfo = {};
 	var lat = req.query.lat;
 	var lng = req.query.lng;
-	publicIp.v4().then(ip => {
-		reqInfo.ip = ip;
-		reqInfo.lat = lat;
-		reqInfo.lng = lng;
-		reqInfo.timestamp = new Date().toISOString();
-		console.log(reqInfo);
-	});
+	reqInfo.ip = req.connection.remoteAddress
+	reqInfo.lat = lat;
+	reqInfo.lng = lng;
+	reqInfo.timestamp = new Date().toISOString();
+	console.log(reqInfo);
 	request('https://api.instagram.com/v1/media/search?lat=' + lat + '&lng=' + lng + '&distance=500&access_token=' + token,
 		function(error, response, body) {
 			res.send(body);
